153477d940e68dbe65468488bf9b1842a79545c8
Ritish jaiswal <ritishjaiswal20@gmail.com>
2021-12-14 17:57:18 +0530

    starting build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([
+    'Take dogs out for a walk',
+    'Take the rubish out ',
+    'hey i am here',
+  ])
+  const [input, setInput] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>hello world</h1>
+      <input value={input}></input>
+      <button>ADD todo</button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
